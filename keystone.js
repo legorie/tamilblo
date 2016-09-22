ecb372ee0bcdb7562446af672e713c51509412df
takujimo <takujimo@localhost.localdomain>
2016-09-22 15:08:03 +0200

    started using VSCode

--- keystone.js
+++ keystone.js
@@ -6,8 +6,6 @@ require('dotenv').config();
 var keystone = require('keystone');
 var handlebars = require('express-handlebars');
 
-
-added branch
 // Initialise Keystone with your project's configuration.
 // See http://keystonejs.com/guide/config for available options
 // and documentation.
